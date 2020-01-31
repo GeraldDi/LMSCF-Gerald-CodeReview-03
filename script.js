c2bb3c623afd456e4c72edbcd08d1345c5931657
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-01-31 22:55:56 +0100

    calculation

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function calculateInsurance(){
 	
-	var country = document.getElementById("country");
-	var hp=document.getElementById("horsepower");
-	var ag = document.getElementById("age");
+	var country = document.getElementById("country").value;
+	var hp=document.getElementById("horsepower").value;
+	var ag = document.getElementById("age").value;
 	var insurance=0;
 	var horsepower=parseInt(hp);
 	var age=parseInt(ag);
 
-	/*zu testzewecken ohne if: if(country=="Austria"){
+	if(country="Austria"){
 		insurance = horsepower*100/age+50;
 
-	}else if(country=="Hungary"){
+	}else if(country="Hungary"){
 		insurance =horsepower*120/age+100;
 
 	}else{
 		insurance =horsepower*150/(age+3)+50;
 
-	}*/
-	insurance = horsepower;
-	return insurance;
+	}
+	
 	console.log(insurance);
 	var endcalc = document.getElementById('resultfield');
 	endcalc.innerHTML = insurance;
@@ -26,12 +25,3 @@ function calculateInsurance(){
 
 var elementNode = document.getElementById('calcbutton');
 elementNode.addEventListener('click', calculateInsurance, false);
-
-var elementNodet = document.getElementById('löschenisttest');
-elementNodet.addEventListener('click', testf, false);
-
-function testf(){
-		var endcalc = document.getElementById('resultfield2');
-		endcalc.innerHTML = "testergebnisXXXXXXXXXXXXX";
-		console.log("terfolgreich")
-}
