2441c705b2bba892bc0c7303a9f4956be9af122e
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-02-01 16:38:27 +0100

    bonus

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 function calculateInsurance(){
 	
-	var country = document.getElementById("country").value;
+	var country = document.getElementById("country");
+	var i = country.selectedIndex;
+	var countryid = country.options[i].value;//to choose the selectedd country
+
 	var hp=document.getElementById("horsepower").value;
 	var ag = document.getElementById("age").value;
 	var insurance=0;
 	var horsepower=parseInt(hp);
 	var age=parseInt(ag);
 
-	if(country="Austria"){
-		insurance = "Insurance cost: " +horsepower*100/age+50;
+	if(countryid=="Austria"){
+		insurance = "Insurance cost: " +Math.round((horsepower*100/age+50)); //bracket is necessary or it will just add the number 50 to the result
 
-	}else if(country="Hungary"){
-		insurance ="Insurance cost: " +horsepower*120/age+100;
+	}else if(countryid=="Hungary"){
+		insurance ="Insurance cost: " +Math.round((horsepower*120/age+100));
 
 	}else{
-		insurance ="Insurance cost: " +horsepower*150/(age+3)+50;
-
+		insurance ="Insurance cost: " +Math.round((horsepower*150/(age+3)+50));
 	}
-	
-	console.log(insurance);
 	var endcalc = document.getElementById('resultfield');
 	endcalc.innerHTML = insurance;
 	endcalc.style.visibility="visible";
-
 }
 
 var elementNode = document.getElementById('calcbutton');
